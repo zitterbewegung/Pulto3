2be2bec0dfb4ea80a48f41d9b8febaf1a78d19e0
zitterbewegung <30265+zitterbewegung@users.noreply.github.com>
2025-09-03 22:55:26 -0500

    moved backend to another place

--- pulto-backend-updated/services/api/app/utils.py
+++
@@ -1,8 +0,0 @@
-
-from app.storage import s3
-from app.config import settings
-
-async def ensure_buckets():
-    for bucket in [settings.S3_BUCKET_ASSETS, settings.S3_BUCKET_NOTEBOOKS]:
-        if not s3.bucket_exists(bucket):
-            s3.make_bucket(bucket)
